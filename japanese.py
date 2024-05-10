ea46dce7de9fb6f18a49aa9ac7c275e68e6071d7
maidcode <141510473+maidcode@users.noreply.github.com>
2024-05-10 09:56:23 +0300

    Update japanese.py

--- japanese.py
+++ japanese.py
@@ -1,46 +1,99 @@
-def batch_replace(s, rules):
-    for k in rules.keys(): s = s.replace(k, rules[k])
-    return s
+__all__ = ["randjap"]
 
-from random import choices, randint
-import random
+from random import sample, randint, getrandbits, choices
 
-def japanese(count=1):
-    syll = ["а", "и", "у", "э", "о", "я", "ю", "ё", "ка", "ки", "ку", "кэ", "ко", "кя", "кю", "кё", "са", "си", "су", "сэ", "со", "ся", "сю", "сё", "та", "ти", "цу", "тэ", "то", "тя", "тю", "тё", "на", "ни", "ну", "нэ", "но", "ня", "ню", "нё", "ха", "хи", "фу", "хэ", "хо", "хя", "хю", "хё", "ма", "ми", "му", "мэ", "мо", "мя", "мю", "мё", "я", "ю", "ё", "ра", "ри", "ру", "рэ", "ро", "ря", "рю", "рё", "ва", "ви", "вэ", "о", "н", "га", "ги", "гу", "гэ", "го", "гя", "гю", "гё", "дза", "дзи", "дзу", "дзэ", "дзо", "дзя", "дзю", "дзё", "да", "дзи", "дзу", "дэ", "до", "дзя", "дзю", "дзё", "ба", "би", "бу", "бэ", "бо", "бя", "бю", "бё", "па", "пи", "пу", "пэ", "по", "пя", "пю", "пё"]
-    yoted = list("яёюые")
-    unyoted = list("аоуиэ")
-    forbidden_ends_of_words = "эы"
+randbool = lambda: getrandbits(1)
 
-    replace_rules = {
-        "ая": "оя",
-        "оё": "ое",
-        "эе": "ое",
-        "яа": "а",
-        "ёо": "о",
-        "юу": "ю",
-        "еэ": "е",
-        "аа": "а",
-        "оо": "о",
-        "уу": "у",
-        "ээ": "э",
-        "яя": "я",
-        "ёё": "ё",
-        "эё": "ё",
-        "юю": "ю",
-        "ии": "и",
-        "ее": "е"
-    }
-    c = 0
-    words = ""
-    while c < count:
-        word = choices(syll, k=randint(2, 4))
-        if word[0] == 'н': word = word[1:] + ['н']
-        if word[-1][-1] in forbidden_ends_of_words: word[-1] = word[-1][:-1] + "а"
-        for i in range(len(word)):
-            if word[i-1][-1] in yoted and word[i][-1] in yoted:
-                word[i] = word[i][:-1] + unyoted[yoted.index(word[i][-1])]
-            while word[i-1][:2] == 'дз' and word[i][:2] == 'дз':
-                word[i] = random.choice(syll)
-        words = f"{words + ', ' if words != '' else ''}{batch_replace(''.join(word), replace_rules)}"
-        c += 1
-    return words
+
+def chance(a: int) -> bool:
+    """One in `a` chance."""
+    return not randint(0, a)
+
+
+# all info taken from NipponGo: Introduction
+romaji = {
+    "consonants": "kstnhmyrwgzdbp",
+    "vowels": "aiueo",
+    "illegal": {
+        # normal
+        "si": "shi",
+        "ti": "chi",
+        "tu": "tsu",
+        "hu": "fu",
+        # illegal glides
+        "yi": "yu",
+        "ye": "yo",
+        # voiced
+        "zi": "ji",
+        "di": "ji",
+        "du": "zu",
+        # illegal w
+        "wo": "o",
+        "wu": "tsu",
+        "we": "de",
+        "wi": "chi",
+    },
+    "doubled": "ptsk",
+    "nasal": "n",
+}
+
+
+def correct(syllable, first=False, alphabet=romaji):
+    illegal = alphabet["illegal"]
+    doubled = alphabet["doubled"]
+
+    if syllable in illegal.keys():
+        return illegal[syllable]
+
+    if not first and syllable[0] in doubled and chance(4):
+        # one in four chance to double the consonant
+        syllable = syllable[0] + syllable
+
+    if chance(6):
+        # one in six chance to add a nasal syllable
+        syllable += alphabet["nasal"]
+
+    return syllable
+
+
+def randjap(
+    alphabet=romaji,
+    min_vowels=3,
+    max_vowels=5,
+    max_consonants=7,
+):
+    """
+    Generate a random (possibly non-existent) Japanese-sounding word.
+    """
+
+    result = ""
+
+    vowels = choices(alphabet["vowels"], k=randint(min_vowels, max_vowels))
+    consonants = sample(
+        alphabet["consonants"], k=randint(len(vowels), max_consonants)
+    )
+
+    first = True
+    if chance(3):
+        first = False
+        result += vowels.pop()
+
+    for con in consonants:
+        if not vowels:
+            if con == alphabet["nasal"]:
+                result += con
+            break
+
+        result += correct(con + vowels.pop(), first)
+        first = False
+    return result
+
+
+if __name__ == "__main__":
+    try:
+        for _ in range(int(input("How many? "))):
+            print(randjap(), end=", ")
+        print("")
+    except Exception as exc:
+        print(f"[ERROR] {exc}")
+        print("")
